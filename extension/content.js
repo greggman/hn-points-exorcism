2ea3906440bf2bf94d7977e3a3d245c16bd96d29
Gregg Tavares <github@greggman.com>
2025-05-03 02:27:17 +0900

    hide karma on user page

--- extension/content.js
+++ extension/content.js
@@ -6,8 +6,11 @@
 }
 
 {
-  const table = document.querySelector('.profileform table');
-  if (table) {
+  const tables = [
+    ...document.querySelectorAll('.profileform table'),
+    ...document.querySelectorAll('#hnmain table'),
+  ];
+  for (const table of tables) {
     [...table.rows].forEach(row => {
       if (row.cells.length >= 2 && row.cells[0].textContent === 'karma:') {
         row.cells[1].textContent = '???';
